556edbecc2b2aa137be56de5d47abdc4735c47a6
mkreive <mkreive@gmail.com>
2022-09-22 09:09:45 +0300

    testing done

--- 01-starting-project/src/util/io.test.js
+++ 01-starting-project/src/util/io.test.js
@@ -1,3 +1,8 @@
 import { it, expect } from 'vitest';
+import writeData from './io';
 
-it('should ', () => {});
+it('should execute the writeFile method', () => {
+    const testData = 'test';
+    const testFilename = 'test.txt';
+    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+});
